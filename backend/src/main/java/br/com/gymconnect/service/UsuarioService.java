4481d9e6defceaab21cc9e88b4b2a583d5373c1c
Ryan da Silva Doriguetto <ryandasilvadoriguetto@gmail.com>
2026-06-12 17:52:59 -0300

    fix(alunos): retorna 409 ao tentar remover aluno com dados associados
    Captura DataIntegrityViolationException em UsuarioService.remover() e
    retorna HTTP 409 com mensagem amigável em português, em vez de lançar
    500. Também remove o bean singleton ResponseModel injetado pelo Spring,
    usando instância local para evitar condição de corrida.

--- backend/src/main/java/br/com/gymconnect/service/UsuarioService.java
+++ backend/src/main/java/br/com/gymconnect/service/UsuarioService.java
@@ -1,6 +1,7 @@
 package br.com.gymconnect.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,23 +11,24 @@ import br.com.gymconnect.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
-    
-    @Autowired
-    private UsuarioRepository ur;
 
     @Autowired
-    private ResponseModel rm;
+    private UsuarioRepository ur;
 
     public Iterable<Usuario> listar(){
         return ur.findAll();
     }
 
     public ResponseEntity<ResponseModel> remover(Long idUsuario){
-        
-        ur.deleteById(idUsuario);
-
-        rm.setMensagem("O Usuario foi removido com sucesso!");
-        return new ResponseEntity<>(rm, HttpStatus.OK);
+        ResponseModel rm = new ResponseModel();
+        try {
+            ur.deleteById(idUsuario);
+            rm.setMensagem("O Usuario foi removido com sucesso!");
+            return new ResponseEntity<>(rm, HttpStatus.OK);
+        } catch (DataIntegrityViolationException e) {
+            rm.setMensagem("Não é possível remover este aluno pois ele possui dados associados (treinos, registros, etc.).");
+            return new ResponseEntity<>(rm, HttpStatus.CONFLICT);
+        }
     }
 
 }
